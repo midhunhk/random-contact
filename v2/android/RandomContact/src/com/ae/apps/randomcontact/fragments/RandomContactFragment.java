171da7d6322468e79050ab676215774db4dc0b0a
midhunhk <midhunhk@gmail.com>
2015-12-28 23:03:34 +0530

    updated app icon, fix for contact user not working

--- v2/android/RandomContact/src/com/ae/apps/randomcontact/fragments/RandomContactFragment.java
+++ v2/android/RandomContact/src/com/ae/apps/randomcontact/fragments/RandomContactFragment.java
@@ -99,7 +99,7 @@ public class RandomContactFragment extends Fragment {
 		mLastContactedLayout.setVisibility(View.GONE);
 
 		// Create the Recycler Adapter
-		mRecyclerAdapter = new ContactRecyclerAdapter(null, R.layout.contact_info_row, mContext);
+		mRecyclerAdapter = new ContactRecyclerAdapter(null, R.layout.contact_info_row, getActivity());
 
 		// Find the RecyclerView and set some properties
 		RecyclerView recyclerView = (RecyclerView) layout.findViewById(android.R.id.list);
