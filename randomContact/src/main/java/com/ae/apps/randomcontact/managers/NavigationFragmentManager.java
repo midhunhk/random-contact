2fb6f355d519c58135492ec7b06d38df22ce294e
midhunhk <midhunhk@gmail.com>
2017-11-04 16:41:45 +0530

    add new icon and contact group

--- randomContact/src/main/java/com/ae/apps/randomcontact/managers/NavigationFragmentManager.java
+++ randomContact/src/main/java/com/ae/apps/randomcontact/managers/NavigationFragmentManager.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.ae.apps.randomcontact.fragments.AboutFragment;
+import com.ae.apps.randomcontact.fragments.ContactGroupFragment;
 import com.ae.apps.randomcontact.fragments.FrequentContactsFragment;
 import com.ae.apps.randomcontact.fragments.RandomContactFragment;
 
@@ -11,64 +12,65 @@ import android.support.v4.app.Fragment;
 
 /**
  * Manages the Fragments that are used in this app
- * 
- * @author Midhun
  *
+ * @author Midhun
  */
 public class NavigationFragmentManager {
 
-	private List<Fragment>	mFragments;
-	
-	private List<String> mItemTitles;
+    private List<Fragment> mFragments;
 
-	/**
-	 * Create and hold on to all fragments that we need
-	 */
-	public NavigationFragmentManager() {
-		mFragments = new ArrayList<Fragment>();
-		mFragments.add(new RandomContactFragment());
-		mFragments.add(new FrequentContactsFragment());
-		mFragments.add(new AboutFragment());
-		
-		// Create the titles, we should be reading them from resources
-		mItemTitles = new ArrayList<String>();
-		mItemTitles.add("Random Contact");
-		mItemTitles.add("Frequent Contacts");
-		mItemTitles.add("About");
-	}
-	
-	/**
-	 * Returns all the titles
-	 * 
-	 * @return
-	 */
-	public List<String> getNavTitles(){
-		return mItemTitles;
-	}
-	
-	/**
-	 * Returns the title of the item at the position
-	 * 
-	 * @param position
-	 * @return
-	 */
-	public String getItemTitle(int position){
-		if(position >= 0 && position < mItemTitles.size()){
-			return mItemTitles.get(position);
-		}
-		return null;
-	}
+    private List<String> mItemTitles;
 
-	/**
-	 * Returns the fragment at the position
-	 * 
-	 * @param position
-	 * @return
-	 */
-	public Fragment getFragmentInstance(int position) {
-		if (position >= 0 && position < mFragments.size()) {
-			return mFragments.get(position);
-		}
-		return null;
-	}
+    /**
+     * Create and hold on to all fragments that we need
+     */
+    public NavigationFragmentManager() {
+        mFragments = new ArrayList<>();
+        mFragments.add(new RandomContactFragment());
+        mFragments.add(new ContactGroupFragment());
+        mFragments.add(new FrequentContactsFragment());
+        mFragments.add(new AboutFragment());
+
+        // Create the titles, we should be reading them from resources
+        mItemTitles = new ArrayList<>();
+        mItemTitles.add("Random Contact");
+        mItemTitles.add("Contact Groups");
+        mItemTitles.add("Frequent Contacts");
+        mItemTitles.add("About");
+    }
+
+    /**
+     * Returns all the titles
+     *
+     * @return nav titles
+     */
+    public List<String> getNavTitles() {
+        return mItemTitles;
+    }
+
+    /**
+     * Returns the title of the item at the position
+     *
+     * @param position position
+     * @return title
+     */
+    public String getItemTitle(int position) {
+        if (position >= 0 && position < mItemTitles.size()) {
+            return mItemTitles.get(position);
+        }
+        return null;
+    }
+
+    /**
+     * Returns the fragment at the position
+     *
+     * @param position position
+     * @return fragment
+     */
+    public Fragment getFragmentInstance(int position) {
+        if (position >= 0 && position < mFragments.size()) {
+            return mFragments.get(position);
+        }
+        return null;
+    }
 }
