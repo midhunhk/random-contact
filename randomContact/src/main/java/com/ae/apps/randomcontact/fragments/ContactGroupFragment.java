a90a86e55e05430587fb4fe62ab7744d96e68ddf
midhunhk <midhunhk@gmail.com>
2017-11-05 17:58:33 +0530

    update layout

--- randomContact/src/main/java/com/ae/apps/randomcontact/fragments/ContactGroupFragment.java
+++ randomContact/src/main/java/com/ae/apps/randomcontact/fragments/ContactGroupFragment.java
@@ -56,12 +56,12 @@ public class ContactGroupFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnListFragmentInteractionListener) {
+        /*if (context instanceof OnListFragmentInteractionListener) {
             mListener = (OnListFragmentInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnListFragmentInteractionListener");
-        }
+        }*/
     }
 
     @Override
