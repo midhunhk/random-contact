27760903d24523e31557484e8e5ef2be7d2e753d
midhunhk <2026817+midhunhk@users.noreply.github.com>
2021-02-13 15:18:27 -0500

    create a brand new project

--- randomContact/src/main/java/com/ae/apps/randomcontact/data/ContactGroup.kt
+++
@@ -1,6 +0,0 @@
-package com.ae.apps.randomcontact.data
-
-/**
- * Model that Represents a ContactGroup
- */
-data class ContactGroup(var id: String, var name: String, var selectedContacts: String)
--- resources/random-contact-blog/source/_posts/An-App-Blog.md
+++
@@ -1,6 +0,0 @@
----
-title: An App Blog
-date: 2017-12-29 01:06:25
-tags: post
----
-This is the first post from the Random Contact App's blog!
