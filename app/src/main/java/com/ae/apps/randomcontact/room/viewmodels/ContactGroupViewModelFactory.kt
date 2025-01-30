16f722e1117077d459f36dee7aad39f6c75f8c48
Midhun Harikumar <2026817+midhunhk@users.noreply.github.com>
2025-01-30 14:59:59 -0500

    Nebulawave (#36)
    * start migration
    
    * fixes #32
    
    * update readme
    
    * version changes
    
    * fix build error
    
    * update
    
    * fixes #35 possibly
    
    * update agp
    
    * dependency updates

--- app/src/main/java/com/ae/apps/randomcontact/room/viewmodels/ContactGroupViewModelFactory.kt
+++ app/src/main/java/com/ae/apps/randomcontact/room/viewmodels/ContactGroupViewModelFactory.kt
@@ -7,7 +7,11 @@ import com.ae.apps.randomcontact.room.repositories.ContactGroupRepository
 class ContactGroupViewModelFactory(private val repository: ContactGroupRepository): ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return ContactGroupViewModel(repository) as T
+        if (modelClass.isAssignableFrom(ContactGroupViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return ContactGroupViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 
 }
